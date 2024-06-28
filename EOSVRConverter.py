a17c7864ca70271a0cd7f8f5dccd623204184099
rspoerri <rspoerri@nouser.org>
2024-06-28 15:36:03 +0200

    improved a bit

--- EOSVRConverter.py
+++ EOSVRConverter.py
@@ -1,3 +1,5 @@
+#!/Users/rspoerri/_REPOSITORIES/_EXTERNAL/EOSVRConverter/eosconverter_venv/bin/python3
+
 import numpy as np
 import cv2
 import sys
@@ -16,11 +18,15 @@ import lut3d
 from functools import partial
 import numpy as np
 from pillow_lut import load_cube_file
+import argparse
+from alive_progress import alive_bar
+from datetime import datetime, timedelta
+import signal, traceback
 
 # download canon lut from  (bottom of the page)
 # https://tools.rodrigopolo.com/canonluts/ 
 
-TOPAZ_BIN = r"C:\Program Files\Topaz Labs LLC\Topaz Sharpen AI\Topaz Sharpen AI.exe"
+# TOPAZ_BIN = r"C:\Program Files\Topaz Labs LLC\Topaz Sharpen AI\Topaz Sharpen AI.exe"
 
 # Code adapted from https://gist.github.com/HViktorTsoi/8e8b0468a9fb07842669aa368382a7df
 # Did some changes to speed up by 33% while nearly not changed the result
@@ -110,6 +116,40 @@ def shadowHighlightSaturationAdjustment(
     return output
 
 
+class BarHandler:
+    bar = None
+    barUpdateMethod = None
+    def __init__(self, *args, **kwargs):
+        self.bar = alive_bar(*args, **kwargs)
+        self.barUpdateMethod = self.bar.__enter__()
+
+    def update(self, *args, **kwargs):
+        self.barUpdateMethod.__call__(*args, **kwargs)
+
+    def __del__(self):
+        self.bar.__exit__(None, None, None)
+        
+        self.barUpdateMethod = None
+        self.bar = None
+    
+    @property
+    def text(self):
+        if self.barUpdateMethod:
+            return self.barUpdateMethod.text
+    @text.setter
+    def text(self, value):
+        if self.barUpdateMethod:
+            self.barUpdateMethod.text = value
+    @property
+    def title(self):
+        if self.barUpdateMethod:
+            return self.barUpdateMethod.title
+    @title.setter
+    def title(self, value):
+        if self.barUpdateMethod:
+            self.barUpdateMethod.title = value
+
+
 # From https://github.com/kylemcdonald/FisheyeToEquirectangular
 class FisheyeToEquirectangular:
     def __init__(self, n=4096, side=3600, blending=0, aperture=1):
@@ -118,10 +158,17 @@ class FisheyeToEquirectangular:
 
         npy_file = f'fisheye-{n}-{side}.npy'
 
+        self.matrix_n = n
+        self.matrix_side = side
+
         if exists(npy_file):
+            print (f"load conversion matrix for size {n} / {side} / {blending} / {aperture} from {npy_file}")
+
             data = np.load(npy_file)
             self.x, self.y = data
         else:
+            print (f"generate conversion matrix for size {n} / {side} / {blending} / {aperture} and save as {npy_file}")
+
             x_samples = np.linspace(0-blending_ratio, 1+blending_ratio, n+blending*2)
             y_samples = np.linspace(-1, 1, n)
 
@@ -163,16 +210,29 @@ class FisheyeToEquirectangular:
             borderMode=border
         )
 
-    def getLeftRightFisheyeImage(self, imgfn):
+    def getLeftRightFisheyeImage(self, imgfn, side):
         try:
             img = cv2.imread(imgfn)
+            # print (f"img.shape {img.shape}")
             h, w, c = img.shape
-            global args
-            # print (f"--- getLeftRightFisheyeImage {h} {w} {c} {args.side} ---")
+            
+            # global conversionArgs
+            
+            # print (f"--- getLeftRightFisheyeImage {h} {w} {c} {side} ---")
             centerLx = w // 4 - 50
             centerRx = w * 3 // 4 + 50
             centerY = h // 2 + 50
-            fisheyeR = int(args.side) // 2
+            fisheyeR = int(3600.0/4096.0*(w // 4)) # 3600 for 4096, 1800 for 2048, should be conversionArgs.side
+
+            # self.matrix_n = n
+            # self.matrix_side = side
+
+            if ((w//2) != self.matrix_n):
+                print (f"Error: probably wrong eyesize!!! size per eye: {w//2} matrix side: {(self.matrix_n)}! add --eyesize={w//2} to parameters")
+
+            # print (f"size per eye: {w/2} fisheye radius: {(fisheyeR)}")
+            # if ((w / 2) < (fisheyeR*2)) or ((w/4) > (fisheyeR*2)):
+            #     print (f"Error: probably invalid image eyesize!!! size per eye: {w/2} fisheye radius: {(fisheyeR)}")
 
             ymr = centerY - fisheyeR
             ypr = centerY + fisheyeR
@@ -186,13 +246,14 @@ class FisheyeToEquirectangular:
             imgR = img[ymr: ypr, rmr: rpr, :]
             return imgL, imgR
         except Exception as e:
-            print (f"--- Error in correctForImage {imgfn} {e} {img.shape} ---")
+            print (f"--- Error in getLeftRightFisheyeImage {imgfn} {e} {img.shape} ---")
 
-    def correctForImage(self, imgfn, outfn, shallAdjust=False):
+    def correctForImage(self, imgfn, outfn, side, shallAdjust=False):
         try:
+            global conversionArgs
             # print (f"--- correctForImage {imgfn} {outfn} ---")
-            imgL, imgR = self.getLeftRightFisheyeImage(imgfn)
-            # print (f"--- imgL {imgL.shape} ---")
+            imgL, imgR = self.getLeftRightFisheyeImage(imgfn, conversionArgs.side)
+            # print (f"--- imgL {imgL.shape} imgR {imgR.shape} ---")
             newimg = self.unwarp_single(imgL)
             newimgR = cv2.rotate(newimg, cv2.ROTATE_180)
             newimg = self.unwarp_single(imgR)
@@ -208,11 +269,11 @@ class FisheyeToEquirectangular:
     # Correct all images under the correct directory in place
     def correctAllImages(self, pool):
         fns = glob('*.jpg')
-        
         pool.starmap(self.correctForImage, tqdm([(fn, fn) for fn in fns]))
+        
         # Use Topaz Sharpen AI to enhance resolution
-        command = [TOPAZ_BIN] + fns
-        process = Popen(command)
+        # command = [TOPAZ_BIN] + fns
+        # process = Popen(command)
         process.wait()
 
     # Extract frames from the video using ffmpeg, and then perform correction for each frame (in place)
@@ -221,10 +282,10 @@ class FisheyeToEquirectangular:
     # We also give another example of directly reading in the video file (not RAW though, could be ALL-I)
     # before color grading, and invoke ffmpeg to do color grading.
     def correctForVideo(self, videofn, pool, args):
-        videofn_stem =Path(videofn).stem
+        videofn_stem = Path(videofn).stem
         video_outdir = os.path.dirname(videofn)
 
-        frames_outdir = f"{videofn_stem}_FRAMES"
+        frames_outdir = os.path.abspath(f"{videofn_stem}_FRAMES")
 
         if not exists(frames_outdir):
             mkdir(frames_outdir)
@@ -232,6 +293,10 @@ class FisheyeToEquirectangular:
         log_outfile_mp4_to_pngs = os.path.join(video_outdir, f"{videofn_stem}-mp4_to_pngs-log.txt")
         png_outdir = os.path.join(frames_outdir, "%05d.png")
         if (args.mp4_to_pngs):
+            progressBar = None
+            fps = None
+            duration = None
+            
             print ("--- mp4_to_pngs ---")
             # Example 1: don't do color grading
             ffmpegCommand = ['ffmpeg', '-i', videofn, '-qscale:v', '2']
@@ -243,32 +308,52 @@ class FisheyeToEquirectangular:
             # Cube files can be downloaded from Canon website.
             # ffmpegCommand = ['ffmpeg', '-i', videofn, '-qscale:v', '2', '-vf', 'lut3d=BT2020_CanonLog3-to-BT709_WideDR_33_FF_Ver.2.0.cube', png_outdir]
             with open(log_outfile_mp4_to_pngs, 'w+') as f:
-                exe = Popen(ffmpegCommand, stdout=PIPE, stderr=STDOUT, universal_newlines=True, text=True)
-                for line in exe.stdout:
-                    # line = line.decode("utf-8")
-                    #   Duration: 00:00:41.58, start: 0.000000, bitrate: 439623 kb/s
-                    if line.lstrip().startswith('Duration:'):
-                        duration = line.lstrip().removeprefix('Duration:').strip().split(' ')[0].rstrip(',')
-                        print (f"duration: {duration}")
-                        # print (f"  '{line}'")
-                    #   Stream #0:0[0x1](und): Video: hevc (Rext) (hvc1 / 0x31637668), yuv420p12le(tv, bt709), 4096x2160 [SAR 1:1 DAR 256:135], 438659 kb/s, 59.94 fps, 59.94 tbr, 60k tbn (default)
-                    if (line.lstrip().startswith('Stream')) and ('fps,' in line):
-                        fps = line.split(' fps,')[0].split(' ')[-1]
-                        print (f"fps: {fps}")
-                        # print (f"'{line}'    '{line.split(' fps,')[-1]}'")
-                    # frame=    0 fps=0.0 q=0.0 size=       0KiB time=N/A bitrate=N/A speed=N/A    
-                    # frame=   12 fps=3.0 q=-0.0 size=N/A time=00:00:00.20 bitrate=N/A speed=0.0494x    
-                    if line.strip().startswith('frame='):
-                        framenum = line.strip().removeprefix('frame=').strip().split(' ')[0]
-                        # print (f'X', end='', flush=True)
+                try:
+                    exe = Popen(ffmpegCommand, stdout=PIPE, stderr=STDOUT, universal_newlines=True, text=True, preexec_fn=os.setsid)
+                    for line in exe.stdout:
+                        # line = line.decode("utf-8")
+                        #   Duration: 00:00:41.58, start: 0.000000, bitrate: 439623 kb/s
+                        if line.lstrip().startswith('Duration:'):
+                            duration_string = line.lstrip().removeprefix('Duration:').strip().split(' ')[0].rstrip(',')
+                            t = datetime.strptime(duration_string, "%H:%M:%S.%f")
+                            duration = timedelta(hours=t.hour, minutes=t.minute, seconds=t.second).total_seconds()
+                            # print (f"duration: {duration_string} in s: {duration}")
+                            # print (f"  '{line}'")
+                        #   Stream #0:0[0x1](und): Video: hevc (Rext) (hvc1 / 0x31637668), yuv420p12le(tv, bt709), 4096x2160 [SAR 1:1 DAR 256:135], 438659 kb/s, 59.94 fps, 59.94 tbr, 60k tbn (default)
+                        if (line.lstrip().startswith('Stream')) and ('fps,' in line):
+                            fps = float(line.split(' fps,')[0].split(' ')[-1])
+                            # print (f"fps: {fps}")
+                            # print (f"'{line}'    '{line.split(' fps,')[-1]}'")
+                        if ((duration != None) and (fps != None) and (progressBar == None)):
+                            max_progress = int(fps * duration)
+                            if args.frames_v:
+                                max_progress = args.frames_v
+                            print (f"progress bar with length {max_progress} (t:{duration} * fps:{fps} or frames_v: {args.frames_v})")
+                            progressBar = BarHandler(manual=True)
+                        # frame=    0 fps=0.0 q=0.0 size=       0KiB time=N/A bitrate=N/A speed=N/A    
+                        # frame=   12 fps=3.0 q=-0.0 size=N/A time=00:00:00.20 bitrate=N/A speed=0.0494x    
+                        if line.strip().startswith('frame='):
+                            # print (f" '{line}'")
+                            framenum = int(line.strip().removeprefix('frame=').strip().split(' ')[0])
+                            # print (f'X{framenum} ', end='', flush=True)
+                            if (progressBar!=None):
+                                progressBar.update(framenum/float(max_progress))
+                        # print (line, end="")
                         # sys.stdout.flush()
-                        print (f"{framenum}")
-                        # print (f" '{line}'")
-                    # print (line, end="")
-                    # sys.stdout.flush()
-                    f.write(line)
-                exe.wait()
-                
+                        f.write(line)
+                    exe.wait()
+                except Exception as err:
+                    print (f"ERROR {err}")
+                    print (traceback.format_exc())
+
+                    print (f"trying to kill all spawned processes!")
+                    os.killpg(os.getpgid(exe.pid), signal.SIGTERM)
+                    
+                    exit(0)
+                finally:
+                    del progressBar
+                    
+        # this cannot handle hdr images...
         if (args.mp4_to_pngs_opencv):
             print ("--- mp4_to_pngs_opencv ---")
             cam = cv2.VideoCapture(str(videofn))
@@ -323,6 +408,7 @@ class FisheyeToEquirectangular:
                 exe = Popen(ffmpegAudioCommand, stdout=f, stderr=STDOUT)
                 exe.wait()
 
+
         # Perform the mapping and adjustment (slow) in parallel
         if (args.png_unwrap):
             print ("--- png_unwrap ---")
@@ -330,6 +416,7 @@ class FisheyeToEquirectangular:
             #pool.starmap(self.correctForImage, tqdm([(fn, fn) for fn in fns]))
             pool.starmap(self.correctForImage, tqdm([(fn, fn, True) for fn in fns]))
         
+
         log_outfile_png_to_mp4 = os.path.join(video_outdir, f"{videofn_stem}-png_to_mp4-log.txt")
         video_out_fname = f'{videofn_stem}_VR.mp4'
         if (args.png_to_mp4):
@@ -346,12 +433,24 @@ class FisheyeToEquirectangular:
             print(f'Processing {fn}...')
             self.correctForVideo(fn, f'{fn.replace(".mp4", "")}_Frames', args)
 
+def is_image(file):
+    return file.lower().endswith(('.png', '.jpg', '.jpeg', '.tiff', '.bmp'))
+
+def is_video(file):
+    return file.lower().endswith(('.mp4', '.avi', '.mov', '.mkv', '.flv', '.wmv'))
+
+def generate_output_filename(input_file):
+    base = os.path.basename(input_file)
+    name, ext = os.path.splitext(base)
+    return os.path.join(os.path.dirname(input_file), f"{name}-converted{ext}")
 
 
-class Args:
+class ConversionArgs:
+    # using ffmpeg to convert the images
     mp4_to_pngs = True
     # is bad, can not handle hdr images
-    mp4_to_pngs_opencv = True
+    mp4_to_pngs_opencv = False
+    
     mp4_to_aac = True
     png_unwrap = True
     png_to_mp4 = True
@@ -360,45 +459,49 @@ class Args:
     n = 4096
     # must be 1800 for images with 4096 x 2160 or 3600 for 8192 x 4320
     side = 3600
-
+    # max number of frames to convert, for testing purposes
     frames_v = None
-    # extremely slow!!! 20sec / frame!!!
+    # extremely slow!!! 20sec per frame!!!
     lut3d = None
 
-# overwrite for 4k video resolution
-args = Args()
-args.n = 2048
-args.side = 1800
-# args.lut3d = "BT2020_CanonLog3-to-BT709_WideDR_33_FF_Ver.2.0.cube"
-# process only 100 frames
-#args.frames_v = 100
+# must be defined globally
+conversionArgs = ConversionArgs()
 
-if __name__ == '__main__':
-    # We don't have a command line interface for now to provide maximum 
-    # efficiency (e.g. no need to intialize FisheyeToEquirectangular every time)
-    # Sample usage:
+# Main function to parse arguments and run the script
+def main():
+    parser = argparse.ArgumentParser(description='Process video and image files')
+
+    # Add arguments with default values from the Args class
+    parser.add_argument('--test_conversion', action='store_true', help='Test conversion')
+    parser.add_argument('--eyesize', type=int, default=4096, help='size of the video/image per eye (usually 4096 or 2048)')
+    parser.add_argument('--frames_v', type=int, help='Number of frames to process from video, starting at the beginning')
+    parser.add_argument('--lut3d', type=str, help='Path to the 3D LUT file')
+    parser.add_argument('input_file', type=str, help='Path to the (image or video) file to convert')
+
+    args = parser.parse_args()
 
     procCount = min(56, cpu_count() // 3 * 2)
     print(f'Creating a process pool with {procCount} processed...')
     pool = Pool(procCount)
 
-    # prepare converter
-    converter = FisheyeToEquirectangular(n=args.n, side=args.side)
-    
-    if (args.test_conversion):
-        converter.correctForImage('./A001C002_240520L4_CANON_FRAMES/00071.png', './00071_corrected.png')
-        #converter.correctForImage('./tmpframes2/00001.png', './tmpframes2/00001_corrected.png')
-        exit (0)
-    
-    converter.correctForVideo(
-        '/Users/rspoerri/_REPOSITORIES/_EXTERNAL/EOSVRConverter/A001C002_240520L4_CANON.mp4',
-        pool=pool, args=args)
+    global conversionArgs
 
-    #converter.correctAllImages(args)
-    #for i in list(reversed(range(4014, 4019))):
-        #fn = f'IMG_{i}.MP4'
-        #newfn = f'IMG_{i}'
-        #converter.correctForVideo(fn, newfn, pool, args)
-    #if False:
-    #    converter.correctAllImages(pool, args)
-    #converter.correctAllVideos()
+    conversionArgs.n = args.eyesize
+    conversionArgs.side = int(3600.0/4096.0*args.eyesize)
+
+    conversionArgs.lut3d = args.lut3d
+    conversionArgs.frames_v = args.frames_v
+
+    # prepare converter
+    converter = FisheyeToEquirectangular(n=conversionArgs.n, side=conversionArgs.side)
+
+    # call the methods to correct the video, images
+    if (is_video(args.input_file)):
+        converter.correctForVideo(args.input_file, pool=pool, args=conversionArgs)
+    elif (is_image(args.input_file)):
+        converter.correctForImage(args.input_file, generate_output_filename(args.input_file))
+    else:
+        print(f"Unsupported file type. {args.input_file}")
+
+if __name__ == '__main__':
+    main()
---
+++ create_venv.command
@@ -0,0 +1,2 @@
+python3 -m venv eosconverter_venv
+eosconverter_venv/bin/python3 -m pip install numpy opencv-python alive_progress tqdm pillow-lut Pillow
